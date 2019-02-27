f04bfbe30083476aed2da7ba7c4354014054c876
xflatcn <505574082@qq.com>
2019-02-27 16:38:00 +0800

    实现多了页签主界面

--- lib/modules/HomePage.dart
+++ lib/modules/HomePage.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   HomePageState createState() => HomePageState();
@@ -9,9 +10,30 @@ class HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        height: 200.0,
+        child: Swiper(
+          itemBuilder: _swiperBuilder,
+          itemCount: 3,
+          pagination: new SwiperPagination(
+              builder: DotSwiperPaginationBuilder(
+                color: Colors.black54,
+                activeColor: Colors.white,
+              )),
+          control: new SwiperControl(),
+          scrollDirection: Axis.horizontal,
+          autoplay: true,
+          onTap: (index) => print('点击了第$index个'),
+        )
     );
+  }
+
+  Widget _swiperBuilder(BuildContext context, int index) {
+    return (Image.network(
+      "http://via.placeholder.com/350x150",
+      fit: BoxFit.fill,
+    ));
   }
 
 }
